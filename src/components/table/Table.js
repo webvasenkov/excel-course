272993e421da5a33cb48681f5a6f450aeaed09b3
webvasenkov <webvasenkov@gmail.com>
2020-12-15 10:16:08 +0300

    code rafactoring

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,7 +1,6 @@
 import {ExcelComponent} from "@core/ExcelComponent";
 import {createTable} from '@/components/table/table.template'
-import {getCurrentCoords, setCurrentCoords} from '@core/utils'
-import {$} from '@core/DOM'
+import {resizeHandler} from '@/components/table/table.resize'
 
 class Table extends ExcelComponent {
     static className = 'excel__table'
@@ -18,29 +17,7 @@ class Table extends ExcelComponent {
     }
 
     onMousedown(event) {
-        if (event.target.dataset.resize) {
-            const $resizer = $(event.target)
-            const $parent = $resizer.closest('[data-type="resizable"]')
-            const type = $resizer.data.resize
-            const coords = $parent.coords
-
-            type === 'col'
-                ? $resizer.$el.classList.add('excel__table-col-resize_selected')
-                : $resizer.$el.classList.add('excel__table-row-resize_selected')
-
-            $resizer.css({opacity: 1})
-            let value
-
-            document.onmousemove = (ev) => {
-                value = getCurrentCoords(ev, type, coords, $resizer)
-            }
-
-            document.onmouseup = () => {
-                setCurrentCoords.call(this, type, $parent, value, $resizer)
-                document.onmousemove = null
-                document.onmouseup = null
-            }
-        }
+        resizeHandler.call(this, event)
     }
 }
 
