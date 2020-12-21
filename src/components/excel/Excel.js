8756eb51d349ff1d1078bb637e8a11694073fd6d
Denis Vasenkov <56324129+webvasenkov@users.noreply.github.com>
2020-12-21 14:28:02 +0300

    finish base logical of table
    * refinish base logical
    
    * refactoring

--- src/components/excel/Excel.js
+++ src/components/excel/Excel.js
@@ -15,10 +15,6 @@ export class Excel {
         this.components = this.components.map(Component => {
             const $el = $.create('div', Component.className)
             const component = new Component($el, optionsObserver)
-            // DEBUG
-            // if(component.name) {
-            //     window['c' + component.name] = component
-            // }
             $el.html(component.toHTML())
             $root.append($el)
             return component
--- src/core/DOM.js
+++ src/core/DOM.js
@@ -81,11 +81,13 @@ class DOM {
     }
 
     addClass(className) {
-        return this.$el.classList.add(className)
+        this.$el.classList.add(className)
+        return this
     }
 
     removeClass(className) {
-        return this.$el.classList.remove(className)
+        this.$el.classList.remove(className)
+        return this
     }
 
     id(parse) {
@@ -117,4 +119,3 @@ $.create = (tagName, className = '') => {
 
     return $(el)
 }
-
